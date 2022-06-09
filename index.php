29c606d45a4cf98ce475cff2139fb0400e87899a
khuk <castoraron255@gmail.com>
2022-06-09 14:54:26 +0300

    Sarahs

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        BEI YA MAZAO TANZANIA
+    </h1>
+    <header>
+        
+    </header>
+    <div class="head1">
+        <table>
+            <thead>
+                <td>
+                    
+                </td>
+                <td>
+                    Name
+                </td>
+                <td>
+                    Price in TSH@
+                </td>
+            </thead>
+        </table>
+        <footer>
+            <div class="footer">
+                
+            </div>
+        </footer>
+
+
+    </div>
+    
+</body>
+</html>
